e2d0eac10b35f3e194bdaa0d45e03c2aac992974
Toto Rubianto <toto.rubianto.17@gmail.com>
2020-05-09 10:38:19 +0700

    Update : edit admin profile

--- App/Views/template.php
+++ App/Views/template.php
@@ -16,7 +16,7 @@
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
 
-      <a class="navbar-brand" href="<?php echo BASE . "/index" ?>">UlumKitchen</a>
+      <a class="navbar-brand" href="<?php echo BASE . "/index" ?>"><b>UlumKitchen</b></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
--- App/Views/templateadmin.php
+++ App/Views/templateadmin.php
@@ -12,166 +12,217 @@
 	<link href="<?php echo BASE; ?>/assets/admin/demo/demo.css" rel="stylesheet" />
 </head>
 <body>
-  <?php if (isset($_SESSION)) {
-    if ($_SESSION['role'] !== "ADMIN") {
-      header("location:". BASE . '/index/login');
-    }
-  } else {
-    header("location:". BASE . '/index/login');
-  }
-  ?>
-  <div class="wrapper ">
-    <div class="sidebar" data-color="blue">
+	<?php if (isset($_SESSION)) {
+		if ($_SESSION['role'] !== "ADMIN") {
+			header("location:". BASE . '/index/login');
+		}
+	} else {
+		header("location:". BASE . '/index/login');
+	}
+	?>
+	<div class="wrapper ">
+		<div class="sidebar" data-color="blue">
       <!--
         Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
-      -->
-      <div class="logo">
-        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
-          CT
-        </a>
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-          Toifatul Ulum
-        </a>
-      </div>
-      <div class="sidebar-wrapper" id="sidebar-wrapper">
-        <ul class="nav">
-          <li>
-            <a href="<?php echo BASE . "/admin/admin" ?>">
-              <i class="now-ui-icons design_app"></i>
-              <p>Admin</p>
-            </a>
-          </li>
-          <li>
-            <a href="<?php echo BASE . "/admin/users" ?>">
-              <i class="now-ui-icons design_bullet-list-67"></i>
-              <p>User</p>
-            </a>
-          </li>
-          <li>
-            <a href="<?php echo BASE . "/admin/recipe" ?>">
-              <i class="now-ui-icons design_bullet-list-67"></i>
-              <p>Recipe</p>
-            </a>
-          </li>
-          <li>
-            <a href="<?php echo BASE . "/admin/transaction" ?>">
-              <i class="now-ui-icons text_caps-small"></i>
-              <p>Transaction</p>
-            </a>
-          </li>
-          <li>
-            <a href="<?php echo BASE . "/admin/tags" ?>">
-              <i class="now-ui-icons text_caps-small"></i>
-              <p>Tag</p>
-            </a>
-          </li>
+    -->
+    <div class="logo">
+    	<a href="http://www.creative-tim.com" class="simple-text logo-mini">
+    		CT
+    	</a>
+    	<a href="http://www.creative-tim.com" class="simple-text logo-normal">
+    		Toifatul Ulum
+    	</a>
+    </div>
+    <div class="sidebar-wrapper" id="sidebar-wrapper">
+    	<ul class="nav">
+    		<li>
+    			<a href="<?php echo BASE . "/admin/admin" ?>">
+    				<i class="now-ui-icons design_app"></i>
+    				<p>Admin</p>
+    			</a>
+    		</li>
+    		<li>
+    			<a href="<?php echo BASE . "/admin/users" ?>">
+    				<i class="now-ui-icons design_bullet-list-67"></i>
+    				<p>User</p>
+    			</a>
+    		</li>
+    		<li>
+    			<a href="<?php echo BASE . "/admin/recipe" ?>">
+    				<i class="now-ui-icons design_bullet-list-67"></i>
+    				<p>Recipe</p>
+    			</a>
+    		</li>
+    		<li>
+    			<a href="<?php echo BASE . "/admin/transaction" ?>">
+    				<i class="now-ui-icons text_caps-small"></i>
+    				<p>Transaction</p>
+    			</a>
+    		</li>
+    		<li>
+    			<a href="<?php echo BASE . "/admin/tags" ?>">
+    				<i class="now-ui-icons text_caps-small"></i>
+    				<p>Tag</p>
+    			</a>
+    		</li>
           <!-- <li class="active-pro">
             <a href="./upgrade.html">
               <i class="now-ui-icons arrows-1_cloud-download-93"></i>
               <p>Toifatul Ulum</p>
             </a>
-          </li> -->
-        </ul>
-      </div>
-    </div>
-    <div class="main-panel" id="main-panel">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
-            </div>
-            <a class="navbar-brand" href="#pablo">User Profile</a>
-          </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-            <span class="navbar-toggler-bar navbar-kebab"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navigation">
-            <form>
-              <div class="input-group no-border">
-                <input type="text" value="" class="form-control" placeholder="Search...">
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <i class="now-ui-icons ui-1_zoom-bold"></i>
-                  </div>
-                </div>
-              </div>
-            </form>
-            <ul class="navbar-nav">
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="now-ui-icons location_world"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Some Actions</span>
-                  </p>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="<?php echo BASE . '/index/action_logout'?>">Logout</a>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!-- End Navbar -->
-      <div class="panel-header panel-header-sm">
-      </div>
-      <?php $this->loadViewInTemplate($viewName, $viewData); ?>
-      <footer class="footer">
-        <div class=" container-fluid ">
-          <nav>
-            <ul>
-              <li>
-                <a href="https://www.creative-tim.com">
-                  Creative Tim
-                </a>
-              </li>
-              <li>
-                <a href="http://presentation.creative-tim.com">
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a href="http://blog.creative-tim.com">
-                  Blog
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div class="copyright" id="copyright">
-            &copy; <script>
-              document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-            </script>, Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-          </div>
-        </div>
-      </footer>
-    </div>
-  </div>
+        </li> -->
+    </ul>
+</div>
+</div>
+<div class="main-panel" id="main-panel">
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
+		<div class="container-fluid">
+			<div class="navbar-wrapper">
+				<div class="navbar-toggle">
+					<button type="button" class="navbar-toggler">
+						<span class="navbar-toggler-bar bar1"></span>
+						<span class="navbar-toggler-bar bar2"></span>
+						<span class="navbar-toggler-bar bar3"></span>
+					</button>
+				</div>
+				<a class="navbar-brand" href="#pablo">User Profile</a>
+			</div>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-bar navbar-kebab"></span>
+				<span class="navbar-toggler-bar navbar-kebab"></span>
+				<span class="navbar-toggler-bar navbar-kebab"></span>
+			</button>
+			<div class="collapse navbar-collapse justify-content-end" id="navigation">
+				<form>
+					<div class="input-group no-border">
+						<input type="text" value="" class="form-control" placeholder="Search...">
+						<div class="input-group-append">
+							<div class="input-group-text">
+								<i class="now-ui-icons ui-1_zoom-bold"></i>
+							</div>
+						</div>
+					</div>
+				</form>
+				<ul class="navbar-nav">
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							<i class="now-ui-icons location_world"></i>
+							<p>
+								<span class="d-lg-none d-md-block">Some Actions</span>
+							</p>
+						</a>
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+							<a href="" class="dropdown-item" data-toggle="modal" data-target="#exampleModal" data-whatever="<?php echo $_SESSION['id'] ?>" data-role="<?php echo $_SESSION['role'] ?>">Edit Profile</a>
+							<a class="dropdown-item" href="<?php echo BASE . '/index/action_logout'?>">Logout</a>
+						</div>
+
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<div class="panel-header panel-header-sm">
+	</div>
+	<?php $this->loadViewInTemplate($viewName, $viewData); ?>
+	<footer class="footer">
+		<div class=" container-fluid ">
+			<nav>
+				<ul>
+					<li>
+						<a href="https://www.creative-tim.com">
+							Creative Tim
+						</a>
+					</li>
+					<li>
+						<a href="http://presentation.creative-tim.com">
+							About Us
+						</a>
+					</li>
+					<li>
+						<a href="http://blog.creative-tim.com">
+							Blog
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<div class="copyright" id="copyright">
+				&copy; <script>
+					document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+				</script>, Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+			</div>
+		</div>
+	</footer>
+</div>
+</div>
+
+
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="exampleModalLabel">Edit Admin</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+
+				<form method="post" action="<?php echo BASE . "/admin/edit" ?>">
+					<input type="text" class="id" hidden name="id">
+					<input type="text" class="role" hidden name="role">
+					<div class="form-group">
+						<label for="recipient-name" class="col-form-label">Username:</label>
+						<input type="text" name="username" class="form-control" id="recipient-name">
+					</div>
+					<div class="form-group">
+						<label for="recipient-name" class="col-form-label">Name:</label>
+						<input type="text" name="name" class="form-control" id="recipient-name">
+					</div>
+					<div class="form-group">
+						<label for="recipient-name" class="col-form-label">Password:</label>
+						<input type="password" name="password" class="form-control" id="recipient-name">
+					</div>
+
+					<div class="form-group">
+						<label for="message-text" class="col-form-label">Email:</label>
+						<input type="email" name="email" class="form-control" id="recipient-name">
+					</div>
+					<div class="modal-footer">
+						<button type="submit" data-toggle="modal" class="btn btn-primary">Simpan</button>
+					</div>
+				</form>
+
+			</div>
+
+		</div>
+	</div>
+</div>
+
 
 </body>
 <script src="<?php echo BASE; ?>/assets/admin/js/core/popper.min.js"></script>
 <script src="<?php echo BASE; ?>/assets/admin/js/core/bootstrap.min.js"></script>
 <script src="<?php echo BASE; ?>/assets/admin/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-<!-- Chart JS -->
 <script src="<?php echo BASE; ?>/assets/admin/js/plugins/chartjs.min.js"></script>
-<!--  Notifications Plugin    -->
 <script src="<?php echo BASE; ?>/assets/admin/js/plugins/bootstrap-notify.js"></script>
-<!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="<?php echo BASE; ?>/assets/admin/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script><!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
+<script src="<?php echo BASE; ?>/assets/admin/js/now-ui-dashboard.min.js?v=1.5.0" type="text/javascript"></script>
 <script src="<?php echo BASE; ?>/assets/admin/demo/demo.js"></script>
 <script>
 	$(document).ready(function() {
-      // Javascript method's body can be found in assets/js/demos.js
-      demo.initDashboardPageCharts();
-
-    });
-  </script>
-
-  </html>
+		demo.initDashboardPageCharts();
+	});
+</script>
+<script type="text/javascript">
+	$('#exampleModal').on('show.bs.modal', function (event) {
+		var button = $(event.relatedTarget)
+		var recipient = button.data('whatever') 
+		var role = button.data('role') 
+		var modal = $(this)
+		console.log(role)
+		modal.find('.modal-body .id').val(recipient)
+		modal.find('.modal-body .role').val(role)
+	})
+</script>
+</html>
